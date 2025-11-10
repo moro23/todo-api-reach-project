1a524f89b654e211bfe6e13207be90f35e34c4fe
moro23 <billhab66@gmail.com>
2025-11-10 19:17:13 +0000

    refactor App component to use class syntax and fetch todos from API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+// src/App.js
+
+// Import React and the Component class
+import React, { Component } from 'react';
+import axios from 'axios'; 
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Define the App component as a class
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      todos: []
+    };
+  
+  }
+
+  componentDidMount() {
+    this.getTodos();
+  }
+ 
+  getTodos() {
+    axios
+      .get('http://127.0.0.1:8000/apis/')
+      .then(res => {
+        this.setState({todos: res.data});
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  // The render method is required for class components and returns what gets displayed
+  render() {
+    return (
+      <div>
+        {/* We map over the 'todos' array in our state to display each one */}
+        {this.state.todos.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <p>{item.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
+// Export the App component so it can be used in other files (like index.js)
 export default App;
